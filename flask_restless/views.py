caa2d38a32f8d2505a3245578b2401afecec931d
Jeff Kehler <jeffrey.kehler@gmail.com>
2014-10-16 14:07:34 +0700

    Extended ProcessingException to include an errors argument.

--- flask_restless/views.py
+++ flask_restless/views.py
@@ -89,14 +89,23 @@ class ProcessingException(HTTPException):
 
     `status_code` is the HTTP status code of the response supplied to the
     client in the case that this exception is raised. `message` is an error
-    message describing the cause of this exception. This message will appear in
-    the JSON object in the body of the response to the client.
-
+    message describing the cause of this exception. `errors` is an optional
+    field giving extended details of the errors. The message and optional
+    errors will appear in the JSON object in the body of the response to
+    the client.
     """
-    def __init__(self, description='', code=400, *args, **kwargs):
+    def __init__(self, description='', code=400, errors=None, *args, **kwargs):
         super(ProcessingException, self).__init__(*args, **kwargs)
         self.code = code
         self.description = description
+        self.errors = errors
+
+    def json_response(self):
+        """Generates a JSON response for the Exception"""
+        message = self.description or str(self)
+        if self.errors is not None:
+            return jsonify(message=message, errors=self.errors)
+        return jsonify(message=message)
 
 
 def _is_msie8or9():
@@ -141,8 +150,7 @@ def catch_processing_exceptions(func):
         except ProcessingException as exception:
             current_app.logger.exception(str(exception))
             status = exception.code
-            message = exception.description or str(exception)
-            return jsonify(message=message), status
+            return exception.json_response(), status
     return decorator
 
 
